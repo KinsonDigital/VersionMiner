bbe8613b68814caa507278f0ebddac7c0b80526d
Calvin Wilkinson <kinsondigital@gmail.com>
2023-06-15 16:43:45 +0100

    ide: add word to dictionary for rider users

--- Testing/VersionMinerTests/GitHubActionTests.cs
+++ Testing/VersionMinerTests/GitHubActionTests.cs
@@ -1,4 +1,4 @@
-// <copyright file="GitHubActionTests.cs" company="KinsonDigital">
+﻿// <copyright file="GitHubActionTests.cs" company="KinsonDigital">
 // Copyright (c) KinsonDigital. All rights reserved.
 // </copyright>
 
