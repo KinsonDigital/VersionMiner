e905e81792ebe22c808c4a5d9a3d64d128672445
Calvin Wilkinson <kinsondigital@gmail.com>
2023-06-15 15:22:10 +0100

    chore: create new exception for when a repository does not exist

---
+++ Testing/VersionMinerTests/Exceptions/RepoDoesNotExistExceptionTests.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="RepoDoesNotExistExceptionTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using FluentAssertions;
+using VersionMiner.Exceptions;
+
+namespace VersionMinerTests.Exceptions;
+
+/// <summary>
+/// Tests the <see cref="RepoDoesNotExistException"/> class.
+/// </summary>
+public class RepoDoesNotExistExceptionTests
+{
+    #region Constructor Tests
+    [Fact]
+    public void Ctor_WithNoParam_CorrectlySetsExceptionMessage()
+    {
+        // Act
+        var exception = new RepoDoesNotExistException();
+
+        // Assert
+        exception.Message.Should().Be("The repository does not exist.");
+    }
+
+    [Fact]
+    public void Ctor_WhenInvokedWithSingleMessageParam_CorrectlySetsMessage()
+    {
+        // Act
+        var exception = new RepoDoesNotExistException("test-message");
+
+        // Assert
+        exception.Message.Should().Be("test-message");
+    }
+
+    [Fact]
+    public void Ctor_WhenInvokedWithMessageAndInnerException_ThrowsException()
+    {
+        // Arrange
+        var innerException = new Exception("inner-exception");
+
+        // Act
+        var deviceException = new RepoDoesNotExistException("test-exception", innerException);
+
+        // Assert
+        deviceException.InnerException.Message.Should().Be("inner-exception");
+        deviceException.Message.Should().Be("test-exception");
+    }
+    #endregion
+}
